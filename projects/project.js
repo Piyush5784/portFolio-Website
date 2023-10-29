584a28b438428cbc2c750b27827241cb95f78702
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2023-10-29 23:35:16 +0530

    Macro Project Added
    Responsive mobile view and macro project added

--- projects/project.js
+++ projects/project.js
@@ -4,12 +4,15 @@ const tongle = document.getElementById("tongle");
 
 const show = document.getElementById("projectParent");
 
+const macro_project_section = document.getElementById("macro-project-section");
+
 
 btnLeft.addEventListener("click",rightClickHandler)
 function leftClickHandler(){
     tongle.style.marginLeft = "153px";
     tongle.style.marginRight = "0";
     show.style.display = "none";
+    macro_project_section.style.display = "block";
     btnRight.style.color = "#fff";
     btnLeft.style.color = "black"
 
@@ -17,9 +20,9 @@ function leftClickHandler(){
 btnRight.addEventListener("click",leftClickHandler)
 function rightClickHandler(){
     tongle.style.marginRight = "151px";
-    tongle.style.marginLeft = "0";
-    
+    tongle.style.marginLeft = "0"; 
     show.style.display = "block";
+    macro_project_section.style.display = "none";
     btnLeft.style.color = "#fff";
     btnRight.style.color = "black";
 }
